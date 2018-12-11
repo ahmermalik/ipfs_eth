1dd227dbf9981bed8c426c6d7e577c664d658c39
Ahmer Malik <ahmer9800@gmail.com>
2018-12-10 19:13:13 -0600

    add web3 and storehash

---
+++ eth-ipfs/src/storehash.js
@@ -0,0 +1,40 @@
+
+
+import web3 from './web3';
+const address = '0xaaaf513f773e0c4a953b1ecd223d1bff520d9adb';
+
+
+const ABI = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+
+
+export default new web3.eth.Contract(ABI, address);
